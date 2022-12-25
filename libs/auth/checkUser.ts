e9c255b8c59981ff6d908ded3e1a431a3aa2829b
BotLove <44952349+botlove282@users.noreply.github.com>
2022-12-25 17:12:46 +0100

    Update checkUser.ts

--- libs/auth/checkUser.ts
+++ libs/auth/checkUser.ts
@@ -1,7 +1,13 @@
 import { queryFirstRes } from "../db/actions/query";
 import verifyKey from "./verifyKey";
 
-export default async function checkUser(apikey: string, user_id:string) {
+export default async function checkUser(apikey: string, user_id?:string) {
+    //if user_id is not provided, get it from the apikey, the user_id should be the first 64 characters of the apikey
+    if (!user_id && apikey.length == 128) {
+        user_id = apikey.slice(0, 64)
+    } else if (!user_id) {
+        return false
+    }
     const queryResUsers = await queryFirstRes("SELECT * FROM users WHERE user_id = ?", [user_id]);
     if (!queryResUsers) {
         return false
