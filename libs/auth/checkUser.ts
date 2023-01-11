b594c8723e276a1dd053fb3c5eb08b8bbc6667a0
BreadCatV2 <BreadCat@breadcat.cc>
2023-01-11 15:08:19 +0100

    auth

--- libs/auth/checkUser.ts
+++ libs/auth/checkUser.ts
@@ -4,16 +4,13 @@ import verifyKey from "./verifyKey";
 
 export default async function checkUser(apikey: string, user_id:string) {
     const conn = await getConnection("Check User");
-    const queryResUsers = await conn.query("SELECT * FROM users WHERE user_id = ?", [user_id]);
+    const queryResUsers = (await conn.query("SELECT * FROM users WHERE user_id = ?", [user_id]))[0]
     await conn.release();
     if (!queryResUsers) {
-        console.log('User not found');
         return false
     }
     const keyValid = await verifyKey(user_id, queryResUsers.apikey, apikey);
     if (!keyValid) {
-        console.log('Invalid API key');
-        console.log('Salt: ' + queryResUsers.apikey)
         return false
     }
     return true
